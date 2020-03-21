64cd6c66e89a800ab7ef3c9db0da6ac584eedad6
alinasmrgn <54818001+alinasmrgn@users.noreply.github.com>
2020-03-21 11:52:21 +0300

    Add files via upload

---
+++ lab4/Calculator.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Calculator : Form
+    {
+        double result = 0;
+        string operation = "";
+        bool isOperationDone = false;
+        bool isEqualed = false;
+        double memory;
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Number_Click(object sender, EventArgs e)
+        {
+            if (isEqualed)
+                isEqualed = false;
+            Button button = (Button)sender;
+            if (Display.Text.Equals("0") || (isOperationDone))
+                Display.Clear();
+            isOperationDone = false;
+            if ((button.Text.Equals(",") && !Display.Text.Contains(",")) || !button.Text.Equals(","))
+            {
+                Display.Text += button.Text;
+            }
+        }
+
+        private void Operator_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (result != 0)
+            {
+                ButtonEqual.PerformClick();
+            }
+            operation = button.Text;
+            result = double.Parse(Display.Text);
+            if (!isEqualed)
+                LabelResult.Text += result + " " + operation + " ";
+            else
+                LabelResult.Text += " " + operation;
+            isOperationDone = true;
+        }
+
+        private void ButtonC_Click(object sender, EventArgs e)
+        {
+            operation = "";
+            LabelResult.Text = "";
+            Display.Text = "0";
+            result = 0;
+        }
+
+        private void ButtonEqual_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            LabelResult.Text += Display.Text + button.Text;
+            try
+            {
+                switch (operation)
+                {
+                    case "+":
+                        Display.Text = (result + double.Parse(Display.Text)).ToString();
+                        break;
+                    case "-":
+                        Display.Text = (result - double.Parse(Display.Text)).ToString();
+                        break;
+                    case "*":
+                        Display.Text = (result * double.Parse(Display.Text)).ToString();
+                        break;
+                    case "/":
+                        if (Display.Text.Equals("0"))
+                            throw new ArithmeticException();
+                        Display.Text = (result / double.Parse(Display.Text)).ToString();
+                        break;
+                    case "mod":
+                        Display.Text = (result % double.Parse(Display.Text)).ToString();
+                        break;
+                    default:
+                        LabelResult.Text = "";
+                        break;
+                }
+            }
+            catch (ArithmeticException ex)
+            {
+                Display.Text = "0";
+                LabelResult.Text = "Division by ";
+            }
+            operation = "";
+            isEqualed = true;
+            LabelResult.Text += Display.Text;
+        }
+
+        private void ButtonMemory_Click(object sender, EventArgs e)
+        {
+            memory = double.Parse(Display.Text);
+            LabelMemory.Text = "Mem: " + memory;
+        }
+
+        private void ButtonMemoryResult_Click(object sender, EventArgs e)
+        {
+            Display.Text += memory.ToString();
+        }
+
+        private void Calculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LabelMemory_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Display_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LabelResult_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
---
+++ lab4/Collection.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Collection
+{
+    public partial class Collection : Form
+    {
+        private List<string> _marks;
+        private static Random random;
+        public delegate int OrderSort(object object1, object object2);
+
+        public Collection()
+        {
+            _marks = new List<string>();
+            random = new Random();
+            InitializeComponent();
+        }
+
+        private void ButtonGenerateCollection_Click(object sender, EventArgs e)
+        {
+            int size;
+            ClearCollections();
+            try
+            {
+                size = int.Parse(TextBoxSize.Text);
+            }
+            catch
+            {
+                size = 5;
+            }
+            FillCollection(size);
+            ListBoxDefault.Items.AddRange(_marks.ToArray());
+        }
+
+        private void ClearCollections()
+        {
+            _marks.Clear();
+            ListBoxDefault.Items.Clear();
+            ListBoxResult.Items.Clear();
+        }
+
+        private void FillCollection(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                _marks.Add("Оценка " + random.Next(0, 10).ToString());
+            }
+        }
+
+        private string[] Sort(string type)
+        {
+            OrderSort orderSort = Comparator;
+            string[] array = _marks.ToArray();
+            string temp = "";
+            return type.Equals("ButtonSort") ? BubbleSort(array, orderSort, temp) : BubbleSortByDescending(array, orderSort, temp);
+        }
+
+        private string[] BubbleSort(string[] array, OrderSort orderSort, string temp)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if ((orderSort(array[j], array[j + 1]) < 0))
+                    {
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+        private string[] BubbleSortByDescending(string[] array, OrderSort orderSort, string temp)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if ((orderSort(array[j], array[j + 1]) > 0))
+                    {
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+        private int Comparator(object object1, object object2)
+        {
+            string str1 = (string)object1;
+            string str2 = (string)object2;
+            return string.Compare(str1, str2);
+        }
+
+        private void Sort_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            FillListBoxResult(Sort(button.Name));
+        }
+
+        private void FillListBoxResult(string[] array)
+        {
+            ListBoxResult.Items.Clear();
+            ListBoxResult.Items.AddRange(array);
+        }
+
+        private void Querry_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            FillListBoxResult(GetQuerry(button.Name));
+        }
+
+        private string[] GetQuerry(string buttonName)
+        {
+            switch(buttonName)
+            {
+                case "ButtonDistinct":
+                    return _marks.Distinct().ToArray();
+                case "ButtonOnlyZero":
+                    return _marks.Where(mark => mark.Contains(" 0")).ToArray();
+                case "ButtonFirstFive":
+                    return _marks.Take(5).ToArray();
+                default:
+                    return _marks.ToArray();
+            }
+        }
+    }
+}
